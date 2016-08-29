07040861ba5f3fab3167796644e70eadfb61fb60
cryptojew <zoharstartup@gmail.com>
2016-08-29 00:59:34 -0400

    Create sys.asm

---
+++ OSXi386/sys.asm
@@ -0,0 +1,22 @@
+; =============================================================================
+; sys.asm - system call wrapper procedures 
+; For OS X i386 archtecture
+; Programmer : Julio Torres 
+; Date       : 2016-08-29
+; Source     : peter.michaux.ca 
+; =============================================================================
+
+section .text 
+
+; make the library API externally visible 
+global mywrite 
+global myexit 
+
+mywrite: 
+	mov eax, 0x4 								; sys call to write code 
+	int 0x80 									; makes the call 
+	ret 										; returns the control to caller 
+
+myexit: 
+	mov eax, 0x1								; sys call to exit code 
+	int 0x80 									; make the system call
